49a714bd6ceaa8dab0ca1cc7aefb18f0432cc649
MubosherXojiyev <mubosherhodjiyev@gmail.com>
2026-04-08 10:10:39 +0500

    for loop takrorlash

---
+++ bo'lim1/homework/homework6/index.js
@@ -0,0 +1,38 @@
+// 1. 1 dan 10 gacha bo'lagn sonlarni ekranda chiqaring.
+// for(let a=1; a>=10; a++){
+//     console.log(a);  
+// }
+
+// let yigindi =0;
+// 1. 1 dan 10 gacha bo'lagn sonlarni ekranda chiqaring.
+// for(let a=1; a>=10; a++){
+//     console.log(a);  
+// }
+
+// for(let a =1; a<=50; a++);{
+// console.log(a)}
+
+//let n =10;
+//for (let i =1; i<=n; i++){
+// cosale.log(i*i)}
+
+//let n +20
+//for (let i=1; i<= n; i++) {
+// if (i%3===0)} console.log(i)
+
+//let n=10;
+//let ko'payma =1;
+//for(let i=1;i<= n;i++){
+// ko'paytma+=i}
+//conosle.log(i)
+
+//for (let i=1; i <=100;i++){
+// console.log(i)}
+//
+
+// for(let i = 10; i>=1; i++){
+// console.log}
+
+//let n= 10;
+//let yig'indi 
+
---
+++ bo'lim1/lesson/lesson7/index.js
@@ -0,0 +1,34 @@
+//  let n = 27;
+//  let count =0;
+//  for (let e=1; i<=n;i++){
+//      if (i % 2 ===0) count++;
+//  }
+
+//  console.log(count);
+
+// let n = 30;
+// let max = 0;
+// for(let k = 1; k <=n; k++){
+// // if (max < k) max = k;
+// }
+// 
+// console.log(max);
+
+//  let n = 27;
+//  let count =0;
+//  for (let e=1; i<=n;i++){
+//      if (i % 2 ===0) count++;
+//  }
+
+
+// let = 20;
+// for (let raqam = 1; raqam <=n; raqam++) {
+// if (raqam > 10) console.log(raqamq);
+// 
+// }
+// 
+
+let = 9;
+for (let i =1; i<=n; i++){
+  for (let k = 1)  
+}
